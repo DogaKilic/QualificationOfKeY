77cdd8f6dd220a35a20e0f57d413789c8948a00e
DogaKilic <87138900+DogaKilic@users.noreply.github.com>
2023-01-12 21:09:11 +0100

    Update and moved abs() and aebLogic() into AEBLogic_state

--- src/main/java/converted/AEBLogic_state.java
+++ src/main/java/converted/AEBLogic_state.java
@@ -1,6 +1,6 @@
 package converted;
 
-public class AEBLogic_state {
+public final class AEBLogic_state {
     //Inputs
     int ttc, fcwTime, pb1Time, pb2Time, fbTime, fbDecel, pb1Decel, pb2Decel;
     boolean stop;
@@ -9,10 +9,119 @@ public class AEBLogic_state {
     //State
     int mode;
 
+    private final int M_DEFAULT = 0;
+    private final int M_FCW = 1;
+    private final int M_PARTIAL_BREAKING_1 = 2;
+    private final int M_PARTIAL_BREAKING_2 = 3;
+    private final int M_FULL_BREAKING = 4;
+
+    /*@ public invariant
+      @ M_DEFAULT == 0 && M_FCW == 1 && M_PARTIAL_BREAKING_1 == 2 && M_PARTIAL_BREAKING_2 == 3 && M_FULL_BREAKING == 4 &&
+      @     (this.mode == M_DEFAULT || this.mode == M_FCW || this.mode == M_PARTIAL_BREAKING_1 || this.mode == M_PARTIAL_BREAKING_2
+      @     || this.mode == M_FULL_BREAKING);
+      @*/
+
     /*@
-      @ ensures mode == Modules.M_DEFAULT;
+      @ ensures mode == this.M_DEFAULT;
       @*/
     public AEBLogic_state() {
-        this.mode = Modules.M_DEFAULT;
+        this.mode = this.M_DEFAULT;
+    }
+
+
+
+    /*@ public normal_behaviour
+      @ requires true;
+      @ ensures x < 0 ==> \result == - x;
+      @ ensures x >= 0 ==> \result == x;
+      @ assignable \nothing;
+      @*/
+    private int /*@ strictly_pure helper @*/ abs(int x) {
+        return (x < 0) ? -x : x;
+    }
+
+    /*@ public normal_behavior
+      @ requires true;
+      @ assignable this.mode, this.aebStatus, this.fcwActivate, this.decel;
+      @ ensures \old(this.mode) == M_DEFAULT ==> this.decel == 0 && this.aebStatus == 0 && this.fcwActivate == 0;
+      @ ensures \old(this.mode) == M_DEFAULT && abs(this.ttc) < this.fcwTime && this.ttc < 0 ==> this.mode == M_FCW;
+      @ ensures \old(this.mode) == M_DEFAULT && !(abs(this.ttc) < this.fcwTime && this.ttc < 0) ==> this.mode == M_DEFAULT;
+      @ ensures \old(this.mode) == M_FCW ==> this.decel == 0 && this.aebStatus == 0 && this.fcwActivate == 1;
+      @ ensures \old(this.mode) == M_FCW && abs(this.ttc) < this.pb1Time && this.ttc < 0 ==> this.mode == M_PARTIAL_BREAKING_1;
+      @ ensures \old(this.mode) == M_FCW && abs(this.ttc) >= this.pb1Time && abs(this.ttc) < (10 * this.fcwTime) / 12 && this.ttc < 0 ==> this.mode == M_DEFAULT;
+      @ ensures \old(this.mode) == M_FCW && !(abs(this.ttc) < this.pb1Time && this.ttc < 0) && !(abs(this.ttc) >= this.pb1Time && abs(this.ttc) < (10 * this.fcwTime) / 12 && this.ttc < 0) ==> this.mode == M_FCW;
+      @ ensures \old(this.mode) == M_PARTIAL_BREAKING_1 ==> this.decel == this.pb1Decel && this.aebStatus == 1 && this.fcwActivate == 1;
+      @ ensures \old(this.mode) == M_PARTIAL_BREAKING_1 && abs(this.ttc) < this.pb2Time && this.ttc < 0 ==> this.mode == M_PARTIAL_BREAKING_2;
+      @ ensures \old(this.mode) == M_PARTIAL_BREAKING_1 && (abs(this.ttc) >= this.pb2Time || this.ttc >= 0) && this.stop ==> this.mode == M_DEFAULT;
+      @ ensures \old(this.mode) == M_PARTIAL_BREAKING_1 && !(abs(this.ttc) < this.pb2Time && this.ttc < 0) && !((abs(this.ttc) >= this.pb2Time || this.ttc >= 0) && this.stop) ==> this.mode == M_PARTIAL_BREAKING_1;
+      @ ensures \old(this.mode) == M_PARTIAL_BREAKING_2 ==> this.decel == this.pb2Decel && this.aebStatus == 2 && this.fcwActivate == 1;
+      @ ensures \old(this.mode) == M_PARTIAL_BREAKING_2 && abs(this.ttc) < this.fbTime && this.ttc < 0 ==> this.mode == M_FULL_BREAKING;
+      @ ensures \old(this.mode) == M_PARTIAL_BREAKING_2 && (abs(this.ttc) >= this.fbTime || this.ttc >= 0) && this.stop ==> this.mode == M_DEFAULT;
+      @ ensures \old(this.mode) == M_PARTIAL_BREAKING_2 && !(abs(this.ttc) < this.fbTime && this.ttc < 0) && !((abs(this.ttc) >= this.fbTime || this.ttc >= 0) && this.stop) ==> this.mode == M_PARTIAL_BREAKING_2;
+      @ ensures \old(this.mode) == M_FULL_BREAKING ==> this.decel == this.fbDecel && this.aebStatus == 3 && this.fcwActivate == 1;
+      @ ensures \old(this.mode) == M_FULL_BREAKING && this.stop ==> this.mode == M_DEFAULT;
+      @ ensures \old(this.mode) == M_FULL_BREAKING && !(this.stop) ==> this.mode == M_FULL_BREAKING;
+      @ ensures (this.mode == this.M_DEFAULT || this.mode == this.M_FCW || this.mode == this.M_PARTIAL_BREAKING_1 || this.mode == this.M_PARTIAL_BREAKING_2 || this.mode == this.M_FULL_BREAKING);
+      @ ensures this.\inv;
+      @*/
+    void aebLogic() {
+        switch (this.mode){
+            case M_DEFAULT:
+                this.aebStatus = 0;
+                this.fcwActivate = 0;
+                this.decel = 0;
+
+                if (abs(this.ttc) < this.fcwTime && this.ttc < 0) {
+                    this.mode = M_FCW;
+                }
+
+                break;
+            case M_FCW:
+                this.aebStatus = 0;
+                this.fcwActivate = 1;
+                this.decel = 0;
+
+                if (abs(this.ttc) < this.pb1Time && this.ttc < 0) {
+                    this.mode = M_PARTIAL_BREAKING_1;
+                } else if (abs(this.ttc) < (10 * this.fcwTime) / 12 && this.ttc < 0) {
+                    this.mode = M_DEFAULT;
+                }
+                break;
+            case M_PARTIAL_BREAKING_1:
+                this.aebStatus = 1;
+                this.fcwActivate = 1;
+                this.decel = this.pb1Decel;
+
+                if (abs(this.ttc) < this.pb2Time && this.ttc < 0) {
+                    this.mode = M_PARTIAL_BREAKING_2;
+                }
+                else if (this.stop) {
+                    this.mode = M_DEFAULT;
+                }
+                break;
+            case M_PARTIAL_BREAKING_2:
+                this.aebStatus = 2;
+                this.fcwActivate = 1;
+                this.decel = this.pb2Decel;
+
+                if (abs(this.ttc) < this.fbTime && this.ttc <0) {
+                    this.mode = M_FULL_BREAKING;
+                }
+                else if (this.stop) {
+                    this.mode = M_DEFAULT;
+                }
+                break;
+            case M_FULL_BREAKING:
+                this.aebStatus = 3;
+                this.fcwActivate = 1;
+                this.decel = this.fbDecel;
+
+                if (this.stop) {
+                    this.mode = M_DEFAULT;
+                }
+                break;
+            default:
+                assert false;
+        }
     }
 }
--- src/main/java/converted/Modules.java
+++ src/main/java/converted/Modules.java
@@ -1,112 +1,6 @@
 package converted;
 
 public class Modules {
-    //globals
-
-
-
-    /*@ public normal_behaviour
-      @ requires true;
-      @ ensures x < 0 ==> \result == - x;
-      @ ensures x >= 0 ==> \result == x;
-      @ assignable \nothing;
-      @*/
-    int /*@ strictly_pure helper @*/ abs(int x) {
-        return (x < 0) ? -x : x;
-    }
-
-
-    static final int M_DEFAULT = 0;
-    static final int M_FCW = 1;
-    static final int M_PARTIAL_BREAKING_1 = 2;
-    static final int M_PARTIAL_BREAKING_2 = 3;
-    static final int M_FULL_BREAKING = 4;
-
-    /*@ public normal_behavior
-      @ requires s != null && s.ttc != null && (s.mode == M_DEFAULT || s.mode == M_FCW ||
-      @                        s.mode == M_PARTIAL_BREAKING_1 || s.mode == M_PARTIAL_BREAKING_2 || s.mode == M_FULL_BREAKING);
-      @ assignable s.mode, s.aebStatus, s.fcwActivate, s.decel;
-      @ ensures \old(s.mode) == M_DEFAULT ==> s.decel == 0 && s.aebStatus == 0 && s.fcwActivate == 0;
-      @ ensures \old(s.mode) == M_DEFAULT && abs(s.ttc) < s.fcwTime && s.ttc < 0 ==> s.mode == M_FCW;
-      @ ensures \old(s.mode) == M_DEFAULT && !(abs(s.ttc) < s.fcwTime && s.ttc < 0) ==> s.mode == M_DEFAULT;
-      @ ensures \old(s.mode) == M_FCW ==> s.decel == 0 && s.aebStatus == 0 && s.fcwActivate == 1;
-      @ ensures \old(s.mode) == M_FCW && abs(s.ttc) < s.pb1Time && s.ttc < 0 ==> s.mode == M_PARTIAL_BREAKING_1;
-      @ ensures \old(s.mode) == M_FCW && abs(s.ttc) >= s.pb1Time && abs(s.ttc) < (10 * s.fcwTime) / 12 && s.ttc < 0 ==> s.mode == M_DEFAULT;
-      @ ensures \old(s.mode) == M_FCW && !(abs(s.ttc) < s.pb1Time && s.ttc < 0) && !(abs(s.ttc) >= s.pb1Time && abs(s.ttc) < (10 * s.fcwTime) / 12 && s.ttc < 0) ==> s.mode == M_FCW;
-      @ ensures \old(s.mode) == M_PARTIAL_BREAKING_1 ==> s.decel == s.pb1Decel && s.aebStatus == 1 && s.fcwActivate == 1;
-      @ ensures \old(s.mode) == M_PARTIAL_BREAKING_1 && abs(s.ttc) < s.pb2Time && s.ttc < 0 ==> s.mode == M_PARTIAL_BREAKING_2;
-      @ ensures \old(s.mode) == M_PARTIAL_BREAKING_1 && (abs(s.ttc) >= s.pb2Time || s.ttc >= 0) && s.stop ==> s.mode == M_DEFAULT;
-      @ ensures \old(s.mode) == M_PARTIAL_BREAKING_1 && !(abs(s.ttc) < s.pb2Time && s.ttc < 0) && !((abs(s.ttc) >= s.pb2Time || s.ttc >= 0) && s.stop) ==> s.mode == M_PARTIAL_BREAKING_1;
-      @ ensures \old(s.mode) == M_PARTIAL_BREAKING_2 ==> s.decel == s.pb2Decel && s.aebStatus == 2 && s.fcwActivate == 1;
-      @ ensures \old(s.mode) == M_PARTIAL_BREAKING_2 && abs(s.ttc) < s.fbTime && s.ttc < 0 ==> s.mode == M_FULL_BREAKING;
-      @ ensures \old(s.mode) == M_PARTIAL_BREAKING_2 && (abs(s.ttc) >= s.fbTime || s.ttc >= 0) && s.stop ==> s.mode == M_DEFAULT;
-      @ ensures \old(s.mode) == M_PARTIAL_BREAKING_2 && !(abs(s.ttc) < s.fbTime && s.ttc < 0) && !((abs(s.ttc) >= s.fbTime || s.ttc >= 0) && s.stop) ==> s.mode == M_PARTIAL_BREAKING_2;
-      @ ensures \old(s.mode) == M_FULL_BREAKING ==> s.decel == s.fbDecel && s.aebStatus == 3 && s.fcwActivate == 1;
-      @ ensures \old(s.mode) == M_FULL_BREAKING && s.stop ==> s.mode == M_DEFAULT;
-      @ ensures \old(s.mode) == M_FULL_BREAKING && !(s.stop) ==> s.mode == M_FULL_BREAKING;
-      @ ensures (s.mode == Modules.M_DEFAULT || s.mode == Modules.M_FCW || s.mode == Modules.M_PARTIAL_BREAKING_1 || s.mode == Modules.M_PARTIAL_BREAKING_2 || s.mode == Modules.M_FULL_BREAKING);
-      @ //ensures s.\inv;
-      @*/
-    void aebLogic(AEBLogic_state s) {
-        switch (s.mode){
-            case M_DEFAULT:
-                s.aebStatus = 0;
-                s.fcwActivate = 0;
-                s.decel = 0;
-
-                if (abs(s.ttc) < s.fcwTime && s.ttc < 0) {
-                    s.mode = M_FCW;
-                }
-
-                break;
-            case M_FCW:
-                s.aebStatus = 0;
-                s.fcwActivate = 1;
-                s.decel = 0;
-
-                if (abs(s.ttc) < s.pb1Time && s.ttc < 0) {
-                    s.mode = M_PARTIAL_BREAKING_1;
-                } else if (abs(s.ttc) < (10 * s.fcwTime) / 12 && s.ttc < 0) {
-                    s.mode = M_DEFAULT;
-                }
-                break;
-            case M_PARTIAL_BREAKING_1:
-                s.aebStatus = 1;
-                s.fcwActivate = 1;
-                s.decel = s.pb1Decel;
-
-                if (abs(s.ttc) < s.pb2Time && s.ttc < 0) {
-                    s.mode = M_PARTIAL_BREAKING_2;
-                }
-                else if (s.stop) {
-                    s.mode = M_DEFAULT;
-                }
-                break;
-            case M_PARTIAL_BREAKING_2:
-                s.aebStatus = 2;
-                s.fcwActivate = 1;
-                s.decel = s.pb2Decel;
-
-                if (abs(s.ttc) < s.fbTime && s.ttc <0) {
-                    s.mode = M_FULL_BREAKING;
-                }
-                else if (s.stop) {
-                    s.mode = M_DEFAULT;
-                }
-                break;
-            case M_FULL_BREAKING:
-                s.aebStatus = 3;
-                s.fcwActivate = 1;
-                s.decel = s.fbDecel;
-
-                if (s.stop) {
-                    s.mode = M_DEFAULT;
-                }
-                break;
-            default:
-                assert false;
-        }
-    }
 
     CycleResult cycleResult = new CycleResult();
     TTCCalculation_state ttc_state = new TTCCalculation_state();
@@ -118,7 +12,7 @@ public class Modules {
     /*@ public normal_behaviour
       @ requires mioDistance != null && mioVelocity != null && egoVelocity != null && mioVelocity != 0 &&
                         AEB_PB1_decel != 0 && AEB_PB2_decel != 0 && AEB_FB_decel != 0 && ttc_state != null && stc_state != null && aeb_state != null && cycleResult != null &&
-                        ttc_state.ttc != null && (aeb_state.mode == M_DEFAULT || aeb_state.mode == M_FCW || aeb_state.mode == M_PARTIAL_BREAKING_1 || aeb_state.mode == M_PARTIAL_BREAKING_2 || aeb_state.mode == M_FULL_BREAKING) && aeb_state.ttc != null;
+                        ttc_state.ttc != null && aeb_state.ttc != null;
       @ assignable cycleResult.mioDistance, cycleResult.mioVelocity, cycleResult.egoVelocity, ttc_state.mioDistance, ttc_state.mioVelocity, cycleResult.collision
                         , stc_state.egoVelocity, stc_state.FB1decel, stc_state.FB2decel, stc_state.FBdecel, ttc_state.collision, ttc_state.ttc
                         , stc_state.FCWStoppingTime, stc_state.PB1StoppingTime, stc_state.PB2StoppingTime, stc_state.FBStoppingTime
@@ -137,7 +31,6 @@ public class Modules {
       @ ensures cycleResult.egoCarStop == egoVelocity < THRESHOLD_VELOCITY_STOP;
       @ ensures aeb_state.stop == cycleResult.egoCarStop;
       @ ensures cycleResult.fcwActivate == aeb_state.fcwActivate && cycleResult.aebStatus == aeb_state.aebStatus && cycleResult.decelaration == aeb_state.decel;
-      @ ensures (aeb_state.mode == Modules.M_DEFAULT || aeb_state.mode == Modules.M_FCW || aeb_state.mode == Modules.M_PARTIAL_BREAKING_1 || aeb_state.mode == Modules.M_PARTIAL_BREAKING_2 || aeb_state.mode == Modules.M_FULL_BREAKING);
       @*/
     void cycle (int mioDistance, int mioVelocity, int egoVelocity) {
         cycleResult.mioDistance = mioDistance;
@@ -166,7 +59,7 @@ public class Modules {
 
         cycleResult.egoCarStop = egoVelocity < THRESHOLD_VELOCITY_STOP;
         aeb_state.stop = cycleResult.egoCarStop;
-        aebLogic(aeb_state);
+        //aebLogic(aeb_state);
 
 
         cycleResult.fcwActivate = aeb_state.fcwActivate;
@@ -184,19 +77,19 @@ public class Modules {
       @*/
     public int nondet_int() {return random.rand();}
 
-    /*@ public normal_behaviour
-      @ requires random.\inv && random != null && this.nondet_int() != null && this.nondet_int() != 0 && AEB_PB1_decel != 0 && AEB_PB2_decel != 0 && AEB_FB_decel != 0 && ttc_state != null && stc_state != null && aeb_state != null && cycleResult != null &&
+    /* public normal_behaviour
+      requires random.\inv && random != null && this.nondet_int() != null && this.nondet_int() != 0 && AEB_PB1_decel != 0 && AEB_PB2_decel != 0 && AEB_FB_decel != 0 && ttc_state != null && stc_state != null && aeb_state != null && cycleResult != null &&
                         ttc_state.ttc != null && (aeb_state.mode == M_DEFAULT || aeb_state.mode == M_FCW || aeb_state.mode == M_PARTIAL_BREAKING_1 || aeb_state.mode == M_PARTIAL_BREAKING_2 || aeb_state.mode == M_FULL_BREAKING) && aeb_state.ttc != null;
-      @ assignable random.previous, cycleResult.mioDistance, cycleResult.mioVelocity, cycleResult.egoVelocity, ttc_state.mioDistance, ttc_state.mioVelocity, cycleResult.collision
+      assignable random.previous, cycleResult.mioDistance, cycleResult.mioVelocity, cycleResult.egoVelocity, ttc_state.mioDistance, ttc_state.mioVelocity, cycleResult.collision
                         , stc_state.egoVelocity, stc_state.FB1decel, stc_state.FB2decel, stc_state.FBdecel, ttc_state.collision, ttc_state.ttc
                         , stc_state.FCWStoppingTime, stc_state.PB1StoppingTime, stc_state.PB2StoppingTime, stc_state.FBStoppingTime
                         , aeb_state.ttc, aeb_state.fcwTime, aeb_state.pb1Time, aeb_state.pb2Time, aeb_state.fbTime, aeb_state.pb1Decel
                         , aeb_state.pb2Decel, aeb_state.fbDecel, cycleResult.egoCarStop, aeb_state.stop
                         , aeb_state.mode, aeb_state.aebStatus, aeb_state.fcwActivate, aeb_state.decel
                         , cycleResult.fcwActivate, cycleResult.aebStatus, cycleResult.decelaration;
-      @ ensures true;
-      @ diverges true;
-      @*/
+       ensures true;
+       diverges true;
+      */
     public void main() {
         int dist;
         int velo1;
